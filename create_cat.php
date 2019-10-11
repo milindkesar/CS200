7d33f45516383401794876827a5d8e288a5953a5
pratikpatil024 <45988062+pratikpatil024@users.noreply.github.com>
2019-10-11 16:59:50 +0530

    Update create_cat.php

--- create_cat.php
+++ create_cat.php
@@ -17,6 +17,33 @@ echo '</tr>';
     Category description: <textarea name="cat_description" /></textarea>
     <input type="submit" value="Add category" />
  </form>
+ 
+ if($_SERVER['REQUEST_METHOD'] != 'POST')
+{
+    //the form hasn't been posted yet, display it
+    echo '<form method='post' action=''>
+        Category name: <input type='text' name='cat_name' />
+        Category description: <textarea name='cat_description' /></textarea>
+        <input type='submit' value='Add category' />
+     </form>';
+}
+else
+{
+    //the form has been posted, so save it
+    $sql = ìINSERT INTO categories(cat_name, cat_description)
+       VALUES('' . mysql_real_escape_string($_POST['cat_name']) . ì',
+             '' . mysql_real_escape_string($_POST['cat_description']) . ì')';
+    $result = mysql_query($sql);
+    if(!$result)
+    {
+        //something went wrong, display the error
+        echo 'Error' . mysql_error();
+    }
+    else
+    {
+        echo 'New category successfully added.';
+    }
+}
 
 include 'footer.php';
 ?>
