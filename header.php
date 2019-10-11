5cb6d02eed24b970b83bd38dcc5b985329dd6f15
Meghana <meghana@iitbhilai.ac.in>
2019-10-12 02:30:15 +0530

    header edited

--- header.php
+++ header.php
@@ -5,18 +5,29 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="description" content="A short description." />
     <meta name="keywords" content="put, keywords, here" />
-    <title>PHP-MySQL forum</title>
+    <title>Forum</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
-<h1>My forum</h1>
-    <div id="wrapper">
-    <div id="menu">
-        <a class="item" href="/forum/index.php">Home</a> -
-        <a class="item" href="/forum/create_topic.php">Create a topic</a> -
-        <a class="item" href="/forum/create_cat.php">Create a category</a>
-         
-        <div id="userbar">
-        <div id="userbar">Hello Example. Not you? Log out.</div>
-    </div>
+<!-- <h1>My forum</h1> -->
+
+    <nav id="wrapper" class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand ">Pied Piper</a>
+        </div>
+        <ul id="menu" class="nav navbar-nav">
+          <li class="item"><a href="/forum/index.php">Home</a></li>
+          <li><a class="item" href="/forum/create_topic.php">Create a Topic</a></li>
+          <li><a class="item" href="/forum/create_cat.php">Create a Category</a></li>
+        </ul>
+        <ul  id="userbar" class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span>Hey Example</a></li>
+            <li><a id="userbar" href="#"><span class="glyphicon glyphicon-log-in"></span> Log out</a></li>
+        </ul>
+      </div>
+
         <div id="content">
